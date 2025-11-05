f12ae4b79f9f51165154f5125804599fffd94e57
Abdulrahman sani <sanuabdulrahaman851@gmail.com>
2025-11-05 08:32:01 +0100

    small change

--- routes/crimeRoutes.js
+++ routes/crimeRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/report", authController.protect, crimeController.reportCrime);
-router.post("/allCrimes", authController.protect, crimeController.getAllCrimes);
+router.get("/allCrimes", authController.protect, crimeController.getAllCrimes);
 router.patch(
   "/crime-update/:reportId",
   authController.protect,
